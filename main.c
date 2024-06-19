d90bad9f5ba57342d42ae091389d009ebba05f69
Sy Tran Dung <sytrancsvn@gmail.com>
2024-06-19 09:28:30 +0700

    initial

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/types.h>
+int main() {
+  u_int8_t *a1;
+  u_int8_t **a2;
+  u_int8_t ***a3;
+  u_int8_t ****a4;
+
+  printf("%ld %ld %ld %ld\n", sizeof(a1), sizeof(a2), sizeof(a3), sizeof(a4));
+  printf("%ld %ld %ld %ld\n", sizeof(*a1), sizeof(**a2), sizeof(***a3),
+         sizeof(***a4));
+
+  u_int32_t *b1;
+  u_int32_t **b2;
+  u_int32_t ***b3;
+  u_int32_t ****b4;
+
+  printf("%ld %ld %ld %ld\n", sizeof(b1), sizeof(b2), sizeof(b3), sizeof(b4));
+  printf("%ld %ld %ld %ld\n", sizeof(*b1), sizeof(**b2), sizeof(***b3),
+         sizeof(***b4));
+
+  return 0;
+}
